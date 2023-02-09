1def1d6351918effcb285747be785fa0256b248e
Daniel Hafner <daniel.hafner90@gmx.de>
2023-02-09 22:19:26 +0100

    Added carla download script

--- scripts/get_carla.sh
+++ scripts/get_carla.sh
@@ -1,2 +1,11 @@
-# TODO create downloader script
+#!/bin/bash
+# Download command: bash ./scripts/get_carla.sh
 
+# Download/unzip images and labels
+d='./' # destination directory
+url=https://github.com/DanielHfnr/Carla-Object-Detection-Dataset/archive/refs/tags/
+version='1.0'
+f='v'$version'.zip'
+echo 'Downloading' $url$f ' ...'
+curl -L $url$f -o $f && unzip -q $f -d $d && rm $f
+mv Carla-Object-Detection-Dataset-$version carla
