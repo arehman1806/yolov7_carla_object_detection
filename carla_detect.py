fa89d424097ea884ed669a3aff51efabb099823b
Abdul Rehman <arehmannasir@outlook.com>
2023-06-30 18:15:52 +0100

    added a detect method to process images from carla

---
+++ carla_detect.py
@@ -0,0 +1,209 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+import numpy as np
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+class CarlaObjectDetector:
+    
+
+    def __init__(self) -> None:
+        # weights = './runs/train/yolov74/weights/best.pt'
+        weights = './runs/train/yolov74/weights/yolov7.pt'
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = attempt_load(weights, map_location=device)  # load FP32 model
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+        parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+        parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+        parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+        parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+        parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+        parser.add_argument('--view-img', action='store_true', help='display results')
+        parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+        parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+        parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+        parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+        parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+        parser.add_argument('--augment', action='store_true', help='augmented inference')
+        parser.add_argument('--update', action='store_true', help='update all models')
+        parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+        parser.add_argument('--name', default='exp', help='save results to project/name')
+        parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+        parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
+        self.opt = parser.parse_args()
+
+        set_logging()
+        self.device = select_device(self.opt.device)
+        self.stride = int(self.model.stride.max())
+        self.imgsz = check_img_size(640, s=self.stride)  # check img_size
+        # print(opt)
+
+    def detect(self, im0s, save_img=False):
+        im0s = np.array(im0s)
+        view_img, save_txt, trace = True, False, False
+        save_img = False
+        webcam = False
+
+        img = self.letterbox(im0s, self.imgsz, stride=self.stride)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        # Directories
+        save_dir = Path(increment_path(Path(self.opt.project) / self.opt.name, exist_ok=self.opt.exist_ok))  # increment run
+        (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+
+        
+        half = self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        # Load model
+        model = self.model
+        stride = int(model.stride.max())  # model stride
+        self.imgsz = check_img_size(self.imgsz, s=stride)  # check img_size
+
+        if trace:
+            model = TracedModel(model, self.device, self.opt.img_size)
+
+        if half:
+            model.half()  # to FP16
+
+        # Second-stage classifier
+        classify = False
+        if classify:
+            modelc = load_classifier(name='resnet101', n=2)  # initialize
+            modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=self.device)['model']).to(self.device).eval()
+
+        # Get names and colors
+        names = model.module.names if hasattr(model, 'module') else model.names
+        colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+
+        # Run inference
+        if self.device.type != 'cpu':
+            model(torch.zeros(1, 3, self.imgsz, self.imgsz).to(self.device).type_as(next(model.parameters())))  # run once
+        old_img_w = old_img_h = self.imgsz
+        old_img_b = 1
+
+        t0 = time.time()
+
+        # original loop was here
+        path = ["a"]*10
+        img = torch.from_numpy(img).to(self.device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Warmup
+        # if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+        #     old_img_b = img.shape[0]
+        #     old_img_h = img.shape[2]
+        #     old_img_w = img.shape[3]
+        #     for i in range(3):
+        #         model(img, augment=self.opt.augment)[0]
+
+        # Inference
+        t1 = time_synchronized()
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=self.opt.augment)[0]
+        t2 = time_synchronized()
+
+        # Apply NMS
+        pred = non_max_suppression(pred, self.opt.conf_thres, self.opt.iou_thres, classes=self.opt.classes, agnostic=self.opt.agnostic_nms)
+        t3 = time_synchronized()
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # okay until here
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), 0
+            else:
+                p, s, im0, frame = "~/IPAB/abc.png", "", im0s, 0 # not sure if count zero is okay
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            # txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            txt_path = "asda.txt"
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    xyxy = [int(x) for x in xyxy]  # Ensure xyxy values are integers
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = f'{names[int(cls)]} {conf:.2f}'
+                        color = tuple(colors[int(cls)])  # Convert the color to a tuple
+                        plot_one_box(xyxy, im0, label=label, color=(0, 255, 0), line_thickness=1)
+
+            # Print time (inference + NMS)
+            # print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
+
+            return im0
+           
+    def letterbox(self, img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+        # Resize and pad image while meeting stride-multiple constraints
+        shape = img.shape[:2]  # current shape [height, width]
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+
+        # Scale ratio (new / old)
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        if not scaleup:  # only scale down, do not scale up (for better test mAP)
+            r = min(r, 1.0)
+
+        # Compute padding
+        ratio = r, r  # width, height ratios
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+        if auto:  # minimum rectangle
+            dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+        elif scaleFill:  # stretch
+            dw, dh = 0.0, 0.0
+            new_unpad = (new_shape[1], new_shape[0])
+            ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+        dw /= 2  # divide padding into 2 sides
+        dh /= 2
+
+        if shape[::-1] != new_unpad:  # resize
+            img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+        return img, ratio, (dw, dh)
+
+
+if __name__ == '__main__':
+    img_path = "./carla/images/test/Town05_004080.png"
+    img_path = "./recording/607.png"
+    img0 = cv2.imread(img_path)
+    obj_detector = CarlaObjectDetector()
+    
+    annotated = obj_detector.detect(img0)
+
+    cv2.imwrite("annotated.png", annotated)
+
+    
